ee6db5adf9dddc7a13281725e026375b3aa36c93
Ryan Lucas <127222115+RyanLCampos@users.noreply.github.com>
2023-04-08 12:19:58 -0300

    Criando projeto

---
+++ Calculadora.js
@@ -0,0 +1,70 @@
+function calcular() {
+    const grau = parseInt(document.getElementById("grau").value);
+    const epsilon = parseFloat(document.getElementById("epsilon").value);
+    const coeficientes = document.getElementById("coeficientes").value.split(",").map(Number);
+
+    let resultado = "";
+    
+    function exibirIntervalos(x, arrayNum, aux, aux_x, precisao) {
+      let raiz = 0;
+    
+      while (x <= 1000) {
+        let func2 = 0;
+
+        for (let j = 0; j < arrayNum.length; j++) {
+          func2 += arrayNum[j] * Math.pow(x, j);
+        }
+
+        if (x > -999 && Math.abs(x - aux_x) > precisao) {
+          if ((aux < 0 && func2 > 0) || (aux > 0 && func2 < 0)) {
+            resultado += "Intervalo: [" + aux_x + "," + x + "]";
+
+            let fdivisao = 1;
+            let divisao = (aux_x + x) / 2;
+
+            while (fdivisao > precisao) {
+              fdivisao = f(divisao, arrayNum);
+
+              if (aux * fdivisao < 0) {
+                x = divisao;
+              } else {
+                aux_x = divisao;
+                aux = fdivisao;
+              }
+
+              divisao = (aux_x + x) / 2;
+            }
+
+            raiz = divisao;
+            resultado += " (Raiz: " + raiz + ")<br>";
+          }
+        }
+
+        aux = func2;
+        aux_x = x;
+        x += 1;
+      }
+    }
+
+    function f(x, arrayNum) {
+      let func = 0;
+
+      for (let j = 0; j < arrayNum.length; j++) {
+        func += arrayNum[j] * Math.pow(x, j);
+      }
+
+      return func;
+    }
+
+    exibirIntervalos(-1000, coeficientes, 0, 0, epsilon);
+    document.getElementById("resultado").innerHTML = resultado;
+}
+
+function toggleInfo() {
+  var info = document.querySelector('.info');
+  if (info.style.display === 'block') {
+    info.style.display = 'none';
+  } else {
+    info.style.display = 'block';
+  }
+}
